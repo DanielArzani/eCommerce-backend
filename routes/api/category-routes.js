eec9cdeaa7170cd505bfe636e8820e1d678299fb
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-01-13 21:05:54 -0500

    create and test crud routes for Category model

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,163 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+    .then((dbCategoryData) => {
+      // I'm using the JSend specification
+      res.status(200).json({
+        status: "success",
+        data: {
+          category: dbCategoryData,
+        },
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        status: "error",
+        message: "Unable to communicate with database",
+        data: {
+          error: err,
+        },
+      });
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ["product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({
+          status: "fail",
+          message: "No category found with that ID",
+        });
+        return;
+      }
+      res.status(200).json({
+        status: "success",
+        data: {
+          category: dbCategoryData,
+        },
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        status: "error",
+        message: "Unable to communicate with database",
+        data: {
+          error: err,
+        },
+      });
+    });
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post("/", (req, res) => {
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((dbCategoryData) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          category: dbCategoryData,
+        },
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        status: "error",
+        message: "Unable to communicate with database",
+        data: {
+          error: err,
+        },
+      });
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put("/:id", (req, res) => {
+  Category.update(
+    { category_name: req.body.category_name },
+    {
+      where: { id: req.params.id },
+    }
+  )
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({
+          status: "fail",
+          message: "No category found with that ID",
+        });
+        return;
+      }
+      res.status(200).json({
+        status: "success",
+        data: {
+          category: dbCategoryData,
+        },
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        status: "error",
+        message: "Unable to communicate with database",
+        data: {
+          error: err,
+        },
+      });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete("/:id", (req, res) => {
+  Category.destroy({
+    where: { id: req.params.id },
+  })
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({
+          status: "fail",
+          message: "No category found with that ID",
+        });
+        return;
+      }
+      res.status(204).json({
+        status: "success",
+        data: {
+          category: null,
+        },
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        status: "error",
+        message: "Unable to communicate with database",
+        data: {
+          error: err,
+        },
+      });
+    });
 });
 
 module.exports = router;
